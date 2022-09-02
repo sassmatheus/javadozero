cb68f5fc42079c635c537f70593228cc879d2304
Matheus Sass <83067499+sassmatheus@users.noreply.github.com>
2022-09-02 09:21:38 -0300

    change locale

--- exercicios/01-estrutura-sequencial/src/ex02/Main.java
+++ exercicios/01-estrutura-sequencial/src/ex02/Main.java
@@ -1,5 +1,6 @@
 package ex02;
 
+import java.util.Locale;
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -8,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
 		//casas decimais conforme exemplos. Considere o valor de pi = 3.14159
+		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
 		
 		double pi = 3.14159;

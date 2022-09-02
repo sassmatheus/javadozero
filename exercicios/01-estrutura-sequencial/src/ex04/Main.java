fdf2698a15a65a8ebd67ba3bc5ab384c4ad67c11
Matheus Sass <83067499+sassmatheus@users.noreply.github.com>
2022-09-02 09:20:35 -0300

    change locale

--- exercicios/01-estrutura-sequencial/src/ex04/Main.java
+++ exercicios/01-estrutura-sequencial/src/ex04/Main.java
@@ -1,6 +1,6 @@
 package ex04;
 
-import java.awt.desktop.SystemSleepEvent;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		//Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e
 		//calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
 		
 		int numFunc;
